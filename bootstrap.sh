d108e3457f4685385646227174dac25c1a4c3af5
Thomas Jongerius <thomas.jongerius@eu.equinix.com>
2017-02-23 08:51:31 +0100

    Update dev env with python libs

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,6 @@
 
 cd /tmp || exit
 sudo apt-get update -y
-sudo apt-get install -y python python-pip python3-pip apache2 git tcpdump
+sudo apt-get install -y python python-pip python3-pip apache2 git tcpdump libxml2-dev libxslt1-dev python-dev python-jinja2 python-ldap python-libxslt1 python-lxml python-webdav python-yaml python-docutils
 sudo pip3 install requests flask
+sudo pip install ncclient
